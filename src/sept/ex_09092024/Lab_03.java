fad67df7d80e6d76dbe1c2240a87bb501fbabfeb
Aryan Pandey <85880673+aryan3139@users.noreply.github.com>
2024-09-29 00:52:44 +0530

    All the files

---
+++ src/sept/ex_09092024/Lab_03.java
@@ -0,0 +1,13 @@
+package sept.ex_09092024;
+
+public class Lab_03 {
+
+    public static void main(String [] args)
+    {
+        //Data Type
+        //Boolean---Data Type
+        boolean are_you_good_person=true;
+        System.out.println(are_you_good_person);
+
+    }
+}
---
+++ src/sept/ex_11092024_DataType/Lab_04.java
@@ -0,0 +1,27 @@
+package sept.ex_11092024_DataType;
+
+public class Lab_04 {
+    public static void main(String [] args)
+    {
+
+        //Variable and Data type
+        // Int,short, byte,long,float,double,boolean,char
+        boolean do_you_have_car=false;
+        byte b=127;
+        int a=20;
+        short c=45;
+        char d='A';
+        long e=1552255525l;
+        float f=45.45f;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(do_you_have_car);
+
+    }
+}
+
