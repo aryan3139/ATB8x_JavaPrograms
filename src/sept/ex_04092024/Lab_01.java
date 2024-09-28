787984737008ec4cde78e7a69099d1111fa426b5
Aryan Pandey <85880673+aryan3139@users.noreply.github.com>
2024-09-28 23:55:33 +0530

    Welcome to the Github

--- src/sept/ex_04092024/Lab_01.java
+++ src/sept/ex_04092024/Lab_01.java
@@ -1,4 +1,10 @@
 package sept.ex_04092024;
 
 public class Lab_01 {
+    public static void main(String [] args)
+    {
+        System.out.println("Hello Java");
+    }
 }
+
+
---
+++ src/sept/ex_06092024/Lab_02.java
@@ -0,0 +1,9 @@
+package sept.ex_06092024;
+
+public class Lab_02 {
+    public static void main(String [] args)
+    {
+        System.out.println("Hello GitHub");
+    }
+}
+
