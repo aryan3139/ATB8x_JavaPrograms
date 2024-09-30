5b887b03ccad31ff65d9a04a8c652280c5a89c8e
Aryan Pandey <85880673+aryan3139@users.noreply.github.com>
2024-10-01 01:54:29 +0530

    All the files

--- src/java_task/ex_13th_sept_2024/Tsk_001.java
+++ src/java_task/ex_13th_sept_2024/Tsk_001.java
@@ -1,4 +1,22 @@
+
 package java_task.ex_13th_sept_2024;
+import java.io.*;
+import java.util.Scanner;
+
 
 public class Tsk_001 {
+    public static void main(String[] args)
+    {
+
+
+        System.out.println("Enter Number:");
+        Scanner s=new Scanner(System.in);
+        int num=s.nextInt();
+        for( int i=1;i<=10;i++)
+        {
+
+            System.out.printf("%d*%d=%d",num,i,num*i);
+            System.out.println();
+        }
+    }
 }
--- src/java_task/ex_13th_sept_2024/Tsk_002.java
+++ src/java_task/ex_13th_sept_2024/Tsk_002.java
@@ -1,4 +1,12 @@
 package java_task.ex_13th_sept_2024;
 
+import java.util.Scanner;
+
 public class Tsk_002 {
-}
+
+    public static void main(String[] args) {
+        // Change here to change output
+
+    }
+    }
+
