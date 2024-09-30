042e3ebdaaac0c67fb6ee696967934c5794e911c
Aryan Pandey <85880673+aryan3139@users.noreply.github.com>
2024-10-01 02:04:52 +0530

    All the files

--- src/java_task/ex_13th_sept_2024/Tsk_001.java
+++ src/java_task/ex_13th_sept_2024/Tsk_001.java
@@ -7,14 +7,12 @@ import java.util.Scanner;
 public class Tsk_001 {
     public static void main(String[] args)
     {
-
-
+        //Program to print the table of number which user will give( int num 10) output will be like this (by using the printf()
         System.out.println("Enter Number:");
         Scanner s=new Scanner(System.in);
         int num=s.nextInt();
         for( int i=1;i<=10;i++)
         {
-
             System.out.printf("%d*%d=%d",num,i,num*i);
             System.out.println();
         }
--- src/java_task/ex_13th_sept_2024/Tsk_002.java
+++ src/java_task/ex_13th_sept_2024/Tsk_002.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 
 public class Tsk_002 {
 
-    public static void main(String[] args) {
-        // Change here to change output
+    public static void main(String[] args)
+    {
+   //small calculator, which supports +, - , , / and %(?) - Modulus operator
+        double a = 34;
+        double b = 10;
+        System.out.println("Addition :"+(a+b));
+        System.out.println("Subtraction :"+ (a-b));
+        System.out.println("Division:"+a/b);
+        System.out.println("Multiplication :"+a*b);
+        System.out.println("Modulas :"+a%b);
 
     }
     }
